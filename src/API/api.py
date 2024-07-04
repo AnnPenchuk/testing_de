0043a2ce2daedbee4e5f9ae2b0391e3041ba3149
AnnPenchuk <annapenchuk95@mail.ru>
2024-07-04 12:05:57 +0300

    test2

---
+++ src/API/api.py
@@ -0,0 +1,26 @@
+import requests
+
+def api_request():
+    users=[]
+    quantity=input('Сколько пользователей Вы хотите получить?')
+    url = "https://randomuser.me/api/?results="
+    url = "".join([url, quantity])
+    print("url:", url)
+
+#
+    response = requests.get(url)
+
+    res=response.json()
+    for x in res['results']:
+        email=x['email']
+        password=x['login']['password']
+        dict = {'email':email, 'password':password}
+        print('email=',email)
+        print('password=', password)
+        users.append(dict)
+
+    print(users)
+    print(email)
+    return users,email
+
+
--- src/main.py
+++ src/main.py
@@ -1,9 +1,28 @@
 # -*- coding: utf-8 -*-
+import os
+import sys
+import logging
+import asyncio
+from API.api import api_request
+from Validation.valid import Users
+
+#from bot import Bot
+#from settings import settings
 
 
-def main():
-    pass
+# Logging settings
+#log_dir = os.path.join(os.getcwd(), "logs")
+#log_file = os.path.join(log_dir, "logfile.log")
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    api_request()
+    Users()
+    logging.basicConfig(level=logging.INFO, filename="py_log.log",filemode="w")
+    logging.debug("A DEBUG Message")
+    logging.info("An INFO")
+    logging.warning("A WARNING")
+    logging.error("An ERROR")
+    logging.critical("A message of CRITICAL severity")
+
+
